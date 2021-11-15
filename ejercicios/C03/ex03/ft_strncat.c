8733829e7d81f3d245808188defb86c81cf96b20
Celularfreak <82029406+celularfreak@users.noreply.github.com>
2021-11-15 12:06:07 +0100

    Add files via upload

---
+++ ejercicios/C03/ex03/ft_strncat.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnunez-m <dnunez-m@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/11 12:06:52 by dnunez-m          #+#    #+#             */
+/*   Updated: 2021/11/15 10:41:57 by dnunez-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	i2;
+
+	i = 0;
+	i2 = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[i2] != '\0' && i2 < nb)
+	{
+		dest[i + i2] = src[i2];
+		i2++;
+	}
+	dest[i + i2] = '\0';
+	return (dest);
+}
