be11986e4e30c200ecf7bcb8bb22a889aaf84594
Celularfreak <celularfreak@hotmail.com>
2022-01-25 10:16:24 +0100

    2 mas

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 12:21:45 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/01/24 17:06:46 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/01/25 09:33:21 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,6 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+char	*ft_strchr(const char *s, int c);
 
 #endif
