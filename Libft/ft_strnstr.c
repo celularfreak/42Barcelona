14babf51d95b51f42bb2cb19601c5cd08f85d3f4
celularfreak <celularfreak@hotmail.com>
2022-01-25 19:21:52 +0100

    strnstr acabado

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -22,5 +22,15 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 		return (s1);
 	if (ft_strlen(s2) > n)
 		return (NULL);
-
+	while (i < n)
+		{
+			if (ft_strncmp((char *)&s1[i], s2, ft_strlen(s2)) == 0)
+			{
+				if (i + ft_strlen(s2) > n)
+					return (NULL);
+				return ((char *)&s1[i]);
+			}
+			i++;
+		}
+		return (NULL);
 }
