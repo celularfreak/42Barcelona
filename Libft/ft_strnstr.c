dfb315aae5f0f5e165dbf14a8506d613e8765a0c
David Nunez merino <dnunez-m@cbr10s2.42barcelona.com>
2022-01-26 12:53:19 +0100

    atoi y strnstr

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -6,31 +6,30 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 15:39:42 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/01/25 16:06:28 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/01/26 11:52:01 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *s1, const char *s2, size_t n)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
+	size_t	c;
 
 	i = 0;
-
-	if (s2 == NULL || ft_strlen(s2) == 0)
-		return (s1);
-	if (ft_strlen(s2) > n)
-		return (NULL);
-	while (i < n)
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] != '\0' && i < len)
+	{
+		c = 0;
+		while ((haystack[i + c] == needle[c] && (i + c) < len))
 		{
-			if (ft_strncmp((char *)&s1[i], s2, ft_strlen(s2)) == 0)
-			{
-				if (i + ft_strlen(s2) > n)
-					return (NULL);
-				return ((char *)&s1[i]);
-			}
-			i++;
+			if (needle[c + 1] == '\0')
+				return ((char *)(&haystack[i]));
+			c++;
 		}
-		return (NULL);
+		i++;
+	}
+	return (NULL);
 }
