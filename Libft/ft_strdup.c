3da55bb78909fe333de560cd7d59e5b7e254229b
Celularfreak <celularfreak@hotmail.com>
2022-01-27 16:50:52 +0100

    strdup

--- Libft/ft_strdup.c
+++ Libft/ft_strdup.c
@@ -6,20 +6,26 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 15:48:50 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/01/26 16:09:29 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/01/27 16:46:19 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *strdup(const char *s)
+char	*ft_strdup(const char *s)
 {
-	char *dest;
+	char	*dest;
+	size_t	i;
 
-	dest = malloc(ft_strlen(*s));
+	i = 0;
+	dest = malloc(sizeof(char) * ((ft_strlen(s)) + 1));
 
+	if (dest == NULL)
+		return (NULL);
+	while (i != (ft_strlen(s)) + 1)
+	{
+		dest[i] = s[i];
+		i++;
+	}
 	return (dest);
-
-
-
 }
