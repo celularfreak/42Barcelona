e84cb33cb6295cab8377e5d4ae518a16f52b135d
Celularfreak <celularfreak@hotmail.com>
2022-01-28 10:05:31 +0100

    putendl

--- Libft/ft_putendl_fd.c
+++ Libft/ft_putendl_fd.c
@@ -1,33 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_ft.c                                     :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/02 15:46:54 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/01/27 21:24:42 by dnunez-m         ###   ########.fr       */
+/*   Created: 2022/01/28 09:08:02 by dnunez-m          #+#    #+#             */
+/*   Updated: 2022/01/28 09:59:26 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putnbr_fd(int n, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
-	if (n == -2147483648)
-		ft_putstr_fd("-2147483648", fd);
-	if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		n = -n;
-	}
-	if (n >= 10)
-	{
-		ft_putnbr_fd(n / 10, fd);
-		n = n % 10;
-	}
-	if (n < 10)
-	{
-		ft_putchar_fd(n + 48, fd);
-	}
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
 }
