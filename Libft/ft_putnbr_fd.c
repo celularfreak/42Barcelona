9952dd402e970d4f6685a05c6fe47cf7707793b6
Celularfreak <celularfreak@hotmail.com>
2022-01-28 10:53:01 +0100

    putnbr

--- Libft/ft_putnbr_fd.c
+++ Libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 15:46:54 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/01/28 10:04:51 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/01/28 10:51:37 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
+	{
 		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
 	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
