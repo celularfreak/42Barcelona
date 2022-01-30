ed8ab3ab8ca74d9cdfba608a7524b9831ecc3475
Celularfreak <celularfreak@hotmail.com>
2022-01-30 12:00:41 +0100

    ft strtrim

--- Libft/ft_memchr.c
+++ Libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 14:58:21 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/01/25 15:36:34 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/01/30 08:23:32 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	while (i < n)
 	{
 		if (((unsigned char *)s)[i] == ((unsigned char)c))
-			return (&((void *)s)[i]);
+			return (((void *)(s + i)));
 		i++;
 	}
 	return (0);
