4f2618da19242acc259b52c0a1b135493e1e00b3
celularfreak <celularfreak@hotmail.com>
2022-02-01 01:32:44 +0100

    varios list

---
+++ Libft/ft_lstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/01 01:18:54 by dnunez-m          #+#    #+#             */
+/*   Updated: 2022/02/01 01:18:54 by dnunez-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "libft.h"
+
+void ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list *i;
+
+	while(*lst)
+	{
+		i = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = i;
+	}
+	*lst = NULL;
+}
