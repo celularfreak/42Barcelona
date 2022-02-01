a21bd08cdb5a61f40ebb6cbcfae633694534f538
David Nunez merino <dnunez-m@cbr4s4.42barcelona.com>
2022-02-01 15:09:45 +0100

    back

--- Libft/ft_lstadd_back.c
+++ Libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 00:57:28 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/02/01 15:00:25 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/02/01 15:04:44 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*i;
 
-	if (!new)
+	/*if (!new)
 		return (NULL);
 	if (!lst)
 	{
 		*lst = new;
 		return (NULL);
-	}
+	}*/
 	i = ft_lstlast(*lst);
 	i->next = new;
 
