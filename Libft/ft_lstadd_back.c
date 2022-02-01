ab3c5ce2362eb7df9c240ec7d057d96bc33b3a98
Celularfreak <celularfreak@hotmail.com>
2022-02-01 15:02:48 +0100

    varios

--- Libft/ft_lstadd_back.c
+++ Libft/ft_lstadd_back.c
@@ -6,26 +6,24 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 00:57:28 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/02/01 12:30:24 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/02/01 15:00:25 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list *i;
-	
+	t_list	*i;
+
 	if (!new)
-		return;
+		return (NULL);
 	if (!lst)
 	{
 		*lst = new;
-		return;
+		return (NULL);
 	}
 	i = ft_lstlast(*lst);
 	i->next = new;
 
 }
-
-
