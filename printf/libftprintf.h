83f7c8948c12b69787397c2bed2c920c41aabaff
David Nunez merino <dnunez-m@cbr10s2.42barcelona.com>
2022-02-09 13:45:07 +0100

    varios

--- printf/libftprintf.h
+++ printf/libftprintf.h
@@ -6,11 +6,25 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 15:05:33 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/02/08 15:08:28 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/02/09 13:44:16 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
 
+typedef struct s_print
+{
+	va_list	args;
+	int		wdt;
+	int		prc;
+	int		zero;
+	int		pnt;
+	int		dash;
+	int		tl;
+	int		sign;
+	int		is_zero;
+	int		perc;
+	int		sp;
+}	t_print;
 #endif
