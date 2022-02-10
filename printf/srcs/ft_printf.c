fe1ce9b15dea83a3723e428fcfd6c0c88db54fff
Celularfreak <celularfreak@hotmail.com>
2022-02-10 15:05:38 +0100

    subida

--- printf/srcs/ft_printf.c
+++ printf/srcs/ft_printf.c
@@ -6,8 +6,33 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 14:25:22 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/02/08 15:08:19 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/02/10 15:05:01 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
- #include "libftprintf.h"
+#include	"libftprintf.h"
+
+t_print	*ft_inistruckt(t_print *tab)
+{
+	tab->wdt = 0;
+	tab->prc = 0;
+	tab->zero = 0;
+	tab->pnt = 0;
+	tab->sign = 0;
+	tab->tl = 0;
+	tab->is_zero = 0;
+	tab->dash = 0;
+	tab->perc = 0;
+	tab->sp = 0;
+	return (tab);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	t_print	*tab;
+
+	tab = (t_print *)malloc(sizeof(t_print));
+	if (!tab)
+		return (-1);
+	ft_initialise_tab(tab);
+}
