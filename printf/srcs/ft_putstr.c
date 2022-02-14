3f187b2b905d180a9da17a020d04fb16f4c6eef0
David Nunez merino <dnunez-m@cbr10s2.42barcelona.com>
2022-02-14 17:01:03 +0100

    se acabo

--- printf/srcs/ft_putstr.c
+++ printf/srcs/ft_putstr.c
@@ -1,44 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/08 14:25:22 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/02/14 17:00:03 by dnunez-m         ###   ########.fr       */
+/*   Created: 2021/11/04 19:22:53 by dnunez-m          #+#    #+#             */
+/*   Updated: 2022/02/14 15:26:36 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"libftprintf.h"
 
-int	ft_printf(const char *format, ...)
+int	ft_putstr(char *s)
 {
-	int		i;
-	int		ret;
-	va_list	args;
-
-	i = -1;
-	ret = 0;
-	va_start(args, format);
-	while (format[++i])
-	{
-		if (format[i] == '%')
-		i = ft_format(args, format[i + 1]);
-		else
-			ret += ft_putchar(&format[i]);
-	}
-	va_end(args);
-	return (ret);
-}
-
-int	ft_format(va_list args, const char *format)
-{
-	if (format == 'c')
-		ft_putchar(va_args(args, int));
-	if (var == 's')
-		ft_putstr(*format);
-	
-		
-
+	if (!s)
+		return ;
+	write(1, s, ft_strlen(s));
+	return (ft_strlen(s));
 }
