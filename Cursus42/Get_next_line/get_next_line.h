3b5d80174b08b61b682dcea505ea72101c5d4460
David Nunez merino <dnunez-m@cbr10s2.42barcelona.com>
2022-02-22 16:03:59 +0100

    subida

--- Cursus42/Get_next_line/get_next_line.h
+++ Cursus42/Get_next_line/get_next_line.h
@@ -6,7 +6,16 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 10:20:50 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/02/22 10:20:52 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/02/22 11:04:13 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifdef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+
+char *get_next_line(int fd);
+
+
+#endif
