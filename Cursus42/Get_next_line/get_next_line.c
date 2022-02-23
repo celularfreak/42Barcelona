9a987df344b58df8b4ecb475d9f0f4c2976e9d23
Celularfreak <celularfreak@hotmail.com>
2022-02-23 16:56:06 +0100

    cambios

--- Cursus42/Get_next_line/get_next_line.c
+++ Cursus42/Get_next_line/get_next_line.c
@@ -6,39 +6,47 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 10:20:42 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/02/22 11:04:22 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/02/23 16:41:01 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-
-char	*get_next_line(int fd)
-{
-char		*line;
-static char	*accu;
-
-if (fd < 0 || BUFFER_SIZE <= 0)
-	return (0);
-accu = funcion1(fd, accu);
-if (!accu)
-	return (NULL);
-line = funcion2(line);
-accu = funcion3(accu);
-return (line);
-}
-
-char *funcion1(int fd, char *accu)
+char	*funcion1(int fd, char *accu)
 {
 	char	*buf;
 	int		i;
 
 	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buf)
+		return (NULL);
 	i = 1;
-	while(!ft_strchr(accu, '\n') && i != 0)
+	while (!ft_strchr(accu, '\n') && i != 0)
 	{
 		i = read(fd, buf, BUFFER_SIZE);
 		if (i == -1)
-		buf[i] = 
-	}	
+		{
+			free(buf);
+			return (NULL);
+		}
+		buf[i] = '\0';
+		accu = ft_strjoin(accu, buf);
+	}
+	free(buf);
+	return (accu);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*accu;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	accu = funcion1(fd, accu);
+	if (!accu)
+		return (NULL);
+	line = funcion2(accu);
+	accu = funcion3(accu);
+	return (line);
 }
--- Cursus42/Get_next_line/get_next_line.h
+++ Cursus42/Get_next_line/get_next_line.h
@@ -6,17 +6,17 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 10:20:50 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/02/22 11:04:13 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/02/23 16:39:41 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
 
-char 	*get_next_line(int fd);
+char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
