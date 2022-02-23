f2d7cce9955c0a3628f08ee70ed150e1ba1c78c2
celularfreak <celularfreak@hotmail.com>
2022-02-23 11:42:39 +0100

    primeros diseños

--- Cursus42/Get_next_line/get_next_line.c
+++ Cursus42/Get_next_line/get_next_line.c
@@ -15,5 +15,30 @@
 
 char	*get_next_line(int fd)
 {
+char		*line;
+static char	*accu;
 
+if (fd < 0 || BUFFER_SIZE <= 0)
+	return (0);
+accu = funcion1(fd, accu);
+if (!accu)
+	return (NULL);
+line = funcion2(line);
+accu = funcion3(accu);
+return (line);
+}
+
+char *funcion1(int fd, char *accu)
+{
+	char	*buf;
+	int		i;
+
+	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	i = 1;
+	while(!ft_strchr(accu, '\n') && i != 0)
+	{
+		i = read(fd, buf, BUFFER_SIZE);
+		if (i == -1)
+		buf[i] = 
+	}	
 }
--- Cursus42/Get_next_line/get_next_line_utils.c
+++ Cursus42/Get_next_line/get_next_line_utils.c
@@ -10,3 +10,51 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	while ((*s != '\0') && (*s != (char)c))
+	{
+		s++;
+	}
+	if (*s == (char)c)
+	{
+		return ((char *)s);
+	}
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dest;
+	int		j;
+	int		i;
+
+	i = -1;
+	j = -1;
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	dest = malloc(sizeof(char) * ((ft_strlen(s1)+(ft_strlen(s2))) + 1));
+	if (!dest)
+		return (NULL);
+	while (s1 != NULL && s1[++j] != '\0')
+		dest[++i] = s1[j];
+	j = -1;
+	while (s2 != NULL && s2[++j] != '\0')
+		dest[++i] = s2[j];
+	dest[++i] = '\0';
+	return (dest);
+}
