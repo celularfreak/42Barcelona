1db598ff8c057a9dfd595b81a8f12aecb5472855
celularfreak <celularfreak@hotmail.com>
2022-02-23 11:42:39 +0100

    primeros diseños

--- Cursus42/Get_next_line/get_next_line.h
+++ Cursus42/Get_next_line/get_next_line.h
@@ -14,8 +14,12 @@
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
+# include <stdlib.h>
 
-char *get_next_line(int fd);
-
+char 	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*funcion1(int fd, char *accu);
 
 #endif
