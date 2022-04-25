19ec8354f2418395d12d58e65f1026f86f6d1283
David Nunez merino <dnunez-m@cbr11s2.42barcelona.com>
2022-04-25 17:01:39 +0200

    otra

---
+++ Push_swap/push_swap.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/25 12:24:08 by dnunez-m          #+#    #+#             */
+/*   Updated: 2022/04/25 15:02:11 by dnunez-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#indef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+int	push_swap(int argc, char **argv);
+
+#endif
