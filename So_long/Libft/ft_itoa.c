c22cc0564f26e0bda0ec99d113876326b32ea02a
David Nunez merino <dnunez-m@car3s3.42barcelona.com>
2022-05-03 11:16:09 +0200

    primera subida

---
+++ So_long/Libft/ft_itoa.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/30 13:13:00 by dnunez-m          #+#    #+#             */
+/*   Updated: 2022/02/15 09:52:17 by dnunez-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_size(int n)
+{	
+	int	len;
+
+	len = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		len++;
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		size;
+	int		i;
+
+	size = ft_size(n);
+	str = ft_calloc(sizeof(char), (size + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	if (n < 0)
+	{
+		str[0] = '-';
+		i = 1;
+	}
+	while (size-- > i)
+	{
+		if (n < 0)
+			str[size] = '0' + n % 10 * (-1);
+		else
+			str[size] = '0' + n % 10;
+		n = n / 10;
+	}
+	return (str);
+}
---
+++ So_long/Libft/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/01 00:31:39 by dnunez-m          #+#    #+#             */
+/*   Updated: 2022/02/01 12:41:31 by dnunez-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*list;
+
+	list = (t_list *)malloc(sizeof(t_list));
+	if (!list)
+		return (NULL);
+	list->content = content;
+	list->next = NULL;
+	return (list);
+}
