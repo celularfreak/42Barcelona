ddcc1205b76dd7455e24e14daa610dddf9cdc277
Celularfreak <celularfreak@hotmail.com>
2022-05-03 11:16:09 +0200

    primera subida

---
+++ So_long/Libft/ft_strlcat.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/15 10:46:54 by dnunez-m          #+#    #+#             */
+/*   Updated: 2022/01/30 16:55:50 by dnunez-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	res_d;
+	unsigned int	res_s;
+
+	i = ft_strlen(dest);
+	j = 0;
+	res_d = ft_strlen(dest);
+	res_s = ft_strlen(src);
+	if (size < 1)
+		return (res_s + size);
+	while (src[j] && i < size - 1)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	if (size < res_d)
+		return (res_s + size);
+	else
+		return (res_d + res_s);
+}
---
+++ So_long/Libft/ft_strnstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/25 15:39:42 by dnunez-m          #+#    #+#             */
+/*   Updated: 2022/01/26 11:52:01 by dnunez-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	c;
+
+	i = 0;
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] != '\0' && i < len)
+	{
+		c = 0;
+		while ((haystack[i + c] == needle[c] && (i + c) < len))
+		{
+			if (needle[c + 1] == '\0')
+				return ((char *)(&haystack[i]));
+			c++;
+		}
+		i++;
+	}
+	return (NULL);
+}
