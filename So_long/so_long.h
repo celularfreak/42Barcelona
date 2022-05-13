761ee3c72606b91517c68633516e92184828ce99
David Nunez merino <dnunez-m@car2s3.42barcelona.com>
2022-05-13 17:11:17 +0200

    mas

--- So_long/so_long.h
+++ So_long/so_long.h
@@ -6,21 +6,22 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 10:32:38 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/05/13 11:02:29 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/05/13 16:47:31 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#indef SO_LONG_H
+#ifndef SO_LONG_H
 # define SO_LONG_H
 
 #include "./mlx/mlx.h"
 #include "./Libft/libft.h"
-#include "./Get_next_line/get_next_line.h"
+//#include "./Get_next_line/get_next_line.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 
 
