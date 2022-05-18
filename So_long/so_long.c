d90d35ab2eaa154ddc4db203d7fa25a6ff652c55
David Nunez merino <dnunez-m@cbr15s1.42barcelona.com>
2022-05-18 22:14:38 +0200

    all

--- So_long/so_long.c
+++ So_long/so_long.c
@@ -6,7 +6,7 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 10:32:44 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/05/16 16:39:01 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/05/18 18:41:38 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,29 @@
 #include "so_long.h"
 
 int	main(int argc, char **argv)
-{
-	if (argc == 1)
+{	
+	int fd;
+	int i;
+	char **map;
+	char *line;
+
+
+		if (argc == 1)
 		return (0);
-	
+	i = 0;
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		exit_error("Error: Map not found\n");
+	map = ft_calloc(sizeof(char *), 1);
+	if (!map)
+		exit_error("Error: Malloc failed\n");
+	while (1){
+		line = get_next_line(fd);
+		if (line != NULL)
+			map = arr_push(map, line);
+		else
+			break;
+		i++;
+	}
+	close(fd);
 }
