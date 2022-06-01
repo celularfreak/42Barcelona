aae07cd48268f4d358d6ea8bbc8e946ca501e6a0
David Nunez merino <dnunez-m@cbr10s2.42barcelona.com>
2022-06-01 20:42:44 +0200

    all

--- So_long/images.c
+++ So_long/images.c
@@ -6,20 +6,56 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 11:27:27 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/05/30 16:49:04 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/06/01 18:42:53 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-t_vars	*ini_img (t_vars *main)
+t_vars	*ini_img (t_vars *master)
 {
 	int size;
 
-	main->ground = mlx_xpm_file_to_image(main->mlx, "./images/ground.xpm", &size, &size);
-	main->sonic = mlx_xpm_file_to_image(main->mlx, "./images/sonic.xpm", &size, &size);
-	main->rocks = mlx_xpm_file_to_image(main->mlx, "./images/rocks.xpm", &size, &size);
-	main->ring = mlx_xpm_file_to_image(main->mlx, "./images/ring.xpm", &size, &size);
-	main->door = mlx_xpm_file_to_image(main->mlx, "./images/door.xpm", &size, &size);
-	return (main);
+	master->ground = mlx_xpm_file_to_image(master->mlx, "img/hierba.xpm", &size, &size);
+	if (!master->ground)
+		write(1, "Error loading ground image\n", 26);
+	
+	master->sonic = mlx_xpm_file_to_image(master->mlx, "img/Sonic.xpm", &size, &size);
+	master->rocks = mlx_xpm_file_to_image(master->mlx, "img/piedra.xpm", &size, &size);
+	master->ring = mlx_xpm_file_to_image(master->mlx, "img/anillo1.xpm", &size, &size);
+	master->door = mlx_xpm_file_to_image(master->mlx, "img/eggman1.xpm", &size, &size);
+
+	return(master);
+}
+
+
+
+void ins_img(t_vars *master, int x , int y)
+{
+
+int i;
+int j;
+
+i = 0;
+
+while(i <= (x -1))
+{
+	j = 0;
+	while(j <= (y - 1))
+	{
+		if (master->map[j][i] == '0')
+			mlx_put_image_to_window(master->mlx, master->win, master->ground, i * SIZE, j * SIZE);
+		if (master->map[j][i] == 'P')
+			mlx_put_image_to_window(master->mlx, master->win, master->sonic, i * SIZE, j * SIZE);
+		if (master->map[j][i] == '1')
+			mlx_put_image_to_window(master->mlx, master->win, master->rocks, i * SIZE, j * SIZE);
+		if (master->map[j][i] == 'C')
+			mlx_put_image_to_window(master->mlx, master->win, master->ring, i * SIZE, j * SIZE);
+		if (master->map[j][i] == 'E')
+			mlx_put_image_to_window(master->mlx, master->win, master->door, i * SIZE, j * SIZE);
+		j++;
+	}
+	i++;
+}
+
 }
--- So_long/so_long.c
+++ So_long/so_long.c
@@ -6,7 +6,7 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 10:32:44 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/05/30 17:07:11 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/06/01 18:43:13 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,15 @@ int	main(int argc, char **argv)
 	if (argc == 1)
 		return (0);
 	master.map = map_opp(argv[1]);
-	ini_img(&master);
-	master.size_w = (ft_strlen(master.map[0]) - 1);
-	master.size_h = (arr_lenght(master.map) - 1);
 	master.mlx = mlx_init();
+	ini_img(&master);
+	if(!master.ground)
+		write(1, "Error loading ground image\n", 26);
+	master.size_w = (ft_strlen(master.map[0]) - 1);
+	master.size_h = (arr_lenght(master.map) - 1);	
 	master.win = mlx_new_window(master.mlx, master.size_w * SIZE, master.size_h * SIZE, "So Long, and Thanks for All the Fish");
+	//mlx_put_image_to_window(&master.mlx, master.win, master.ground, 0, 0);
+	ins_img(&master, master.size_w, master.size_h);
 	mlx_loop(master.mlx);
 	return (0);
 }
