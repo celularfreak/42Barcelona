61343804ff60f3c33a8b739e3fa101691ba1bada
David Nunez merino <dnunez-m@cbr10s2.42barcelona.com>
2022-06-20 13:10:41 +0200

    test

--- So_long/so_long.h
+++ So_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 10:32:38 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/06/15 10:37:15 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/06/20 13:06:16 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,9 @@ t_vars	*ini_img(t_vars *master);
 void	ins_img(t_vars *master, int x, int y);
 int		key_press(int key, t_character *character, t_vars *master);
 void	put_image(t_vars *master, void *img, int x, int y);
+int		rectangle_test(char **map);
+int		close_test(char **map);
+int		item_count(char **map, char item);
 //int		events(int keycode, t_vars *master);
 
 #endif
