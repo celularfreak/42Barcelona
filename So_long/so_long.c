e159ef0a0ce8740e375bdc21518a7697e690a8c9
celularfreak <celularfreak@hotmail.com>
2022-07-07 09:12:39 +0200

    main comprobacion extension archivo

--- So_long/so_long.c
+++ So_long/so_long.c
@@ -6,7 +6,7 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 10:32:44 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/06/21 11:51:03 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/07/07 09:05:55 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,27 @@
 int	main(int argc, char **argv)
 {	
 	t_vars		master;
+	char		*end;
 
+	end = ft_strrchr(argv[1], '.');
 	if (argc == 1)
 		return (0);
-	master.map = map_opp(argv[1]);
-	map_tester(master.map);
-	master.mlx = mlx_init();
-	ini_img(&master);
-	if (!master.ground)
-		write(1, "Error loading ground image\n", 26);
-	master.size_w = (ft_strlen(master.map[0]) - 1);
-	master.size_h = arr_lenght(master.map);
-	master.win = mlx_new_window(master.mlx, master.size_w * SIZE,
-			master.size_h * SIZE, "So Long, and Thanks for All the Fish");
-	ins_img(&master, master.size_w, master.size_h);
-	//mlx_hook(master.win, 2, 0, events, &master);
-	mlx_loop(master.mlx);
+	if (ft_strncmp(end, ".ber", 3) == 0)
+	{
+		master.map = map_opp(argv[1]);
+		map_tester(master.map);
+		master.mlx = mlx_init();
+		ini_img(&master);
+		if (!master.ground)
+			write(1, "Error loading ground image\n", 26);
+		master.size_w = (ft_strlen(master.map[0]) - 1);
+		master.size_h = arr_lenght(master.map);
+		master.win = mlx_new_window(master.mlx, master.size_w * SIZE,
+				master.size_h * SIZE, "So Long, and Thanks for All the Fish");
+		ins_img(&master, master.size_w, master.size_h);
+		//mlx_hook(master.win, 2, 0, events, &master);
+		mlx_loop(master.mlx);
+	}
+	else
+		write(1, "Error: Wrong file type\n", 23);
 }
