25312f930565fe8ad702b9704a36c3c12c5c3116
celularfreak <celularfreak@hotmail.com>
2022-07-08 15:27:48 +0200

    cosillas

--- So_long/movements.c
+++ So_long/movements.c
@@ -6,33 +6,56 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 10:35:54 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/07/07 12:07:01 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/07/08 15:27:26 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	key_press(int keycode, t_character *character, t_vars *master)
+void	player_pos(char **map, t_vars *master)
 {
-	//char	new_pos;
+	int	i;
+	int	j;
 
-	if (keycode == 13 || keycode == 126) //w
-		//new_pos = master->map[character->x - 1][character->y];
-		write(1, "w", 1);
-	if (keycode == 0 || keycode == 123) //a
-		//new_pos = master->map[character->x + 1][character->y]; 
-		write(1, "a", 1);
-	if (keycode == 1 || keycode == 125)//s
-		//new_pos = master->map[character->x][character->y - 1];
-		write(1, "s", 1);
-	if (keycode == 2 || keycode == 124)//d
-		//new_pos = master->map[character->x][character->y + 1];
-		write(1, "d", 1);
-	if (keycode == 53)//esc
+	i = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
 		{
-			mlx_destroy_window(master->mlx, master->win);
-			exit(0);
+			if (map[i][j] == 'P')
+			{
+				i = master->player_pos_x;
+				j = master->player_pos_y;
+			}
+			j++;
 		}
+		i++;
+	}
+}
+
+char	key_press(int keycode, t_vars *master)
+{
+	if (keycode == 13 || keycode == 126)
+		
+		master->player_pos_x++;
+		return ("W");
+	if (keycode == 0 || keycode == 123)
+		return ("A");
+	if (keycode == 1 || keycode == 125)
+		return ("S");
+	if (keycode == 2 || keycode == 124)
+		return ("D");
+	if (keycode == 53)
+	{
+		mlx_destroy_window(master->mlx, master->win);
+		exit(0);
+	}
 	return (0);
+
+}
+
+void map_mov(t_vars *master)
+{
 
 }
--- So_long/so_long.h
+++ So_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 10:32:38 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/07/07 10:30:01 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/07/08 15:22:41 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,6 @@
 #include 	<sys/uio.h>
 #include 	<string.h>
 
-typedef struct s_character
-{
-	int				x;
-	int				y;
-}					t_character;
-
 typedef struct s_vars
 {
 	void			*ground;
@@ -44,7 +38,9 @@ typedef struct s_vars
 	int				size_w;
 	int				size_h;
 	int				item_count;
-	t_character		character;
+	int				player_pos_x;
+	int				player_pos_y;
+
 }					t_vars;
 
 int		arr_lenght(char **arr);
@@ -58,6 +54,7 @@ int		rectangle_test(char **map);
 int		close_test(char **map);
 int		item_test(char **map, char item);
 void	map_tester(char **map);
-int		key_press(int keycode, t_character *character, t_vars *master);
+//int		key_press(int keycode, t_vars *master);
+void	player_pos(char **map, t_vars *master);
 
 #endif
