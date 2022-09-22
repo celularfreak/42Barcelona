5f530bbba6f32ccf35498b92ae9691611022f87d
celularfreak <celularfreak@hotmail.com>
2022-09-22 18:45:31 +0200

    argv_check y double_check

--- Push_swap/src/argv_check.c
+++ Push_swap/src/argv_check.c
@@ -6,7 +6,7 @@
 /*   By: dnunez-m <dnunez-m@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 11:24:53 by dnunez-m          #+#    #+#             */
-/*   Updated: 2022/09/20 21:46:40 by dnunez-m         ###   ########.fr       */
+/*   Updated: 2022/09/22 18:36:15 by dnunez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,34 @@ static int	ft_strcmp(char *s1, char *s2)
 		++s2;
 	}
 	return (*s1 - *s2);
-}	
+}
 
-int	argv_check(int argc, char **argv)
+static int	check_doubles(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (argc > 1)
+	{
+		j = 1;
+		while (argc > 1)
+		{
+			if (ft_strcmp(argv[i], argv[j]) == 0 && i != j)
+			{
+				write(1, "Error", 5);
+				return (1);
+			}
+			argc--;
+			j++;
+		}
+		argc--;
+		i++;
+	}
+	return (0);
+}
+
+static int	check_valid_arg(int argc, char **argv)
 {
 	int		i;
 	int		temp;
@@ -40,5 +65,14 @@ int	argv_check(int argc, char **argv)
 		argc--;
 		i++;
 	}
+	return (0);
+}
+
+int	argv_check(int argc, char **argv)
+{
+	if (check_doubles(argc, argv) == 1)
+		return (1);
+	if (check_valid_arg(argc, argv) == 1)
+		return (1);
 	return (0);
 }
